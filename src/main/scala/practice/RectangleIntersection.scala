ed1bedd9190a83c27389e36af564020deb625ecb
Kate Fulton <katesuzannefulton@gmail.com>
2016-10-01 16:50:21 -0700

    Additional tests for rectangle overlap

--- src/main/scala/practice/RectangleIntersection.scala
+++ src/main/scala/practice/RectangleIntersection.scala
@@ -1,24 +1,28 @@
 package practice
 
 class RectangleIntersection {
-  def findIntersection(rectangle1: Rectangle, rectangle2: Rectangle): Rectangle = {
+  def findIntersection(rectangle1: Rectangle, rectangle2: Rectangle): Option[Rectangle] = {
+
     val newLeftX = if (rectangle1.left_x > rectangle2.left_x) rectangle1.left_x else rectangle2.left_x
     val newBottomY = if (rectangle1.bottom_y > rectangle2.bottom_y) rectangle1.bottom_y else rectangle2.bottom_y
 
     val newRectangleTopY =
-      if (rectangle1.bottom_y + rectangle1.height < rectangle2.bottom_y + rectangle1.height) {
+      if (rectangle1.bottom_y + rectangle1.height < rectangle2.bottom_y + rectangle2.height) {
         rectangle1.bottom_y + rectangle1.height
-      } else rectangle2.bottom_y + rectangle1.height
+      } else rectangle2.bottom_y + rectangle2.height
 
     val newRectangleRightX =
-      if (rectangle1.left_x + rectangle1.width < rectangle2.left_x + rectangle1.width) {
+      if (rectangle1.left_x + rectangle1.width < rectangle2.left_x + rectangle2.width) {
         rectangle1.left_x + rectangle1.width
-      } else rectangle2.left_x + rectangle1.width
+      } else rectangle2.left_x + rectangle2.width
 
     val newHeight = newRectangleTopY - newBottomY
     val newWidth = newRectangleRightX - newLeftX
 
-    Rectangle(newLeftX, newBottomY, newWidth, newHeight)
+    if (newHeight < 0 || newWidth < 0)
+      None
+    else
+      Some(Rectangle(newLeftX, newBottomY, newWidth, newHeight))
   }
 }
 
--- src/test/scala/practice/RectangleIntersectionSpec.scala
+++ src/test/scala/practice/RectangleIntersectionSpec.scala
@@ -5,16 +5,47 @@ import org.scalatest.{FlatSpec, Matchers}
 class RectangleIntersectionSpec extends FlatSpec with Matchers {
   val rectangleIntersection = new RectangleIntersection
 
-  val rectangle1 = Rectangle(1,3,4,2)
-  val rectangle2 = Rectangle(2,3,3,4)
-  val rectangleIntersection1 = Rectangle(2,3,3,2)
+  "findIntersection" should "find intersection" in {
+    val rectangle1 = Rectangle(1,3,4,2)
+    val rectangle2 = Rectangle(2,3,3,4)
+    val rectangleIntersection1 = Some(Rectangle(2,3,3,2))
 
-  rectangleIntersection.findIntersection(rectangle1, rectangle2) shouldBe rectangleIntersection1
+    rectangleIntersection.findIntersection(rectangle1, rectangle2) shouldBe rectangleIntersection1
+  }
+
+  it should "return None if there is no intersection" in {
+    val rectangle1 = Rectangle(1,3,4,2)
+    val rectangle2 = Rectangle(6,9,2,2)
+
+    rectangleIntersection.findIntersection(rectangle1, rectangle2) shouldBe None
+  }
+
+  it should "return the smaller rectangle if it is completely inside the larger rectangle" in {
+    val rectangle1 = Rectangle(1,3,4,4)
+    val rectangle2 = Rectangle(1,3,2,2)
+    val rectangleIntersection1 = Some(Rectangle(1,3,2,2))
+
+    rectangleIntersection.findIntersection(rectangle1, rectangle2) shouldBe rectangleIntersection1
+  }
+
+//  it should "handle the case where rectangles only share one edge" in {
+//    val rectangle1 = Rectangle(1,3,2,2)
+//    val rectangle2 = Rectangle(3,4,2,2)
+//    val rectangleIntersection1 = Some(Rectangle(3,4,1,1))
+//
+//    rectangleIntersection.findIntersection(rectangle1, rectangle2) shouldBe rectangleIntersection1
+//  }
 }
 
 
 
 /*
+6 [][][][]
+5 [][][][]
+4 [x][x][]
+3 [x][x][]
+  1 2 3
+
 [2,3][2,4][2,5][2,6]
 [1,3][1,4][1,5][1,6]
 
