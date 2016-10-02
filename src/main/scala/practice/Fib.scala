29d2d2a094210b46988174ae4fc7d0fa04c84d7f
Kate Fulton <katesuzannefulton@gmail.com>
2016-10-01 18:06:13 -0700

    Additional non-recursive fib logic solutions

--- src/main/scala/practice/Fib.scala
+++ src/main/scala/practice/Fib.scala
@@ -6,6 +6,24 @@ class Fib {
     case 1 => 1
     case _ => getFibValue(n-1) + getFibValue(n-2)
   }
+
+  def getFibValueNR(n:Int) = {
+    val nRange = 3 to n
+    lazy val (a,b) = nRange.foldLeft((0,1))((z:Tuple2[Int,Int],f:Int) =>
+      (z._2, z._1 + z._2))
+
+    if (n == 0) 0
+    else if (n == 1) 1
+    else a+b
+  }
+
+  def getFibValueStream(n: Int): Int = {
+    lazy val fibs: Stream[Int] = 1 #:: fibs.scanLeft(1)(_ + _)
+    n match {
+      case 0 => 0
+      case _ =>   fibs.take(n).toList.last
+    }
+  }
 }
 
 /*
--- src/test/scala/practice/FibSpec.scala
+++ src/test/scala/practice/FibSpec.scala
@@ -3,13 +3,30 @@ package practice
 import org.scalatest.{FlatSpec, Matchers}
 
 class FibSpec  extends FlatSpec with Matchers {
+  val fib = new Fib
+
   "getFibValue" should "return the correct value" in {
-    val fib = new Fib
     fib.getFibValue(0) shouldBe 0
     fib.getFibValue(1) shouldBe 1
     fib.getFibValue(2) shouldBe 1
     fib.getFibValue(3) shouldBe 2
     fib.getFibValue(4) shouldBe 3
+  }
+
+  "getFibValueNR" should "return the correct value" in {
+    fib.getFibValueNR(0) shouldBe 0
+    fib.getFibValueNR(1) shouldBe 1
+    fib.getFibValueNR(2) shouldBe 1
+    fib.getFibValueNR(3) shouldBe 2
+    fib.getFibValueNR(4) shouldBe 3
+  }
+
+  "getFibValueStream" should "return the correct value" in {
+    fib.getFibValueStream(0) shouldBe 0
+    fib.getFibValueStream(1) shouldBe 1
+    fib.getFibValueStream(2) shouldBe 1
+    fib.getFibValueStream(3) shouldBe 2
+    fib.getFibValueStream(4) shouldBe 3
   }
 }
 
